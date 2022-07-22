59eba39e20e699b07dd55ad69edb265915c56f31
GideokLee <76939190+GideokLee@users.noreply.github.com>
2022-07-22 16:18:07 +0900

    [S07P12D105-180][AOS] chore : Dagger2 설정

---
+++ AOS/app/src/main/java/com/ssafy/sellerb/di/AppModule.kt
@@ -0,0 +1,13 @@
+package com.ssafy.sellerb.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class AppModule {
+
+    @Binds
+    abstract fun bindContext(application: Application): Context
+}
--- AOS/app/src/main/java/com/ssafy/sellerb/di/MainActivityModule.kt
+++ AOS/app/src/main/java/com/ssafy/sellerb/di/MainActivityModule.kt
@@ -1,12 +1,12 @@
-package com.ssafy.sellerb.di
-
-import com.ssafy.sellerb.MainActivity
-import dagger.Module
-import dagger.android.ContributesAndroidInjector
-
-@Suppress("unused")
-@Module
-abstract class MainActivityModule {
-    @ContributesAndroidInjector(modules = [MainActivityModule::class])
-    abstract fun contributeMainActivity(): MainActivity
-}
+//package com.ssafy.sellerb.di
+//
+//import com.ssafy.sellerb.MainActivity
+//import dagger.Module
+//import dagger.android.ContributesAndroidInjector
+//
+//@Suppress("unused")
+//@Module
+//abstract class MainActivityModule {
+//    @ContributesAndroidInjector(modules = [MainActivityModule::class])
+//    abstract fun contributeMainActivity(): MainActivity
+//}
