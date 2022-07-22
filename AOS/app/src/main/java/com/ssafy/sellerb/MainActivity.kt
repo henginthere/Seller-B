95c9c60b96e6b5c80a62e72cba3fd989980ac029
GideokLee <76939190+GideokLee@users.noreply.github.com>
2022-07-22 16:46:12 +0900

    [S07P12D105-180][AOS] chore : Dagger2 nav 등록

--- AOS/app/src/main/java/com/ssafy/sellerb/MainActivity.kt
+++ AOS/app/src/main/java/com/ssafy/sellerb/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ssafy.sellerb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HasAndroidInjector{
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun androidInjector() = dispatchingAndroidInjector
 }
--- AOS/app/src/main/java/com/ssafy/sellerb/SellerB.kt
+++ AOS/app/src/main/java/com/ssafy/sellerb/SellerB.kt
@@ -1,13 +1,24 @@
 package com.ssafy.sellerb
 
+import android.app.Activity
+import android.app.Application
+import com.ssafy.sellerb.di.AppInjector
 import com.ssafy.sellerb.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
 
-class SellerB : DaggerApplication(), HasAndroidInjector {
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
-        DaggerAppComponent.builder().application(this).build()
+class SellerB : Application(), HasAndroidInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
+    override fun onCreate() {
+        super.onCreate()
+        AppInjector.init(this)
+    }
+
+    override fun androidInjector(): DispatchingAndroidInjector<Any> = dispatchingAndroidInjector
 }
