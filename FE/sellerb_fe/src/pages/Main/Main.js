408c8ebc67b88aafeddff8a30d61526b3a92019b
신혜연 <tgd_5753@naver.com>
2022-07-23 14:42:34 +0900

    [S07P12D105-102] [FE] feat : 처음 Main화면 UI

--- FE/sellerb_fe/src/pages/Main/Main.js
+++ FE/sellerb_fe/src/pages/Main/Main.js
@@ -1,8 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+import './Main.css'
+
+// import material ui
+import { styled } from "@mui/material/styles"
+import  TextField  from '@mui/material/TextField';
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/Checkbox';
+
 
 function Main() {
+
+  // const MyTextField = style
+  let [id, setId] = useState("");
+
+  const onIdHandler = (e)=>{
+    console.log(e.target.value)
+    setId(e.target.value)
+  }
+
+  const onLoginButtonHandler = () =>{
+    
+  }
+
   return (
-    <div>Main</div>
+    <>
+    <div className='main-body'>
+    {/* main화면 왼쪽 */}
+    <div className='main-left'>
+      <div className='big-sellerb'>SellerB</div>
+      <div className='main-subtitle'>오늘의 sellerB가 되어 보세요</div>
+      <div className='main-form-wrapper'>
+        {/* Form start */}
+        <form className='main-form'>
+          <TextField 
+            className='main-TextField'
+            sx={{
+              borderColor: 'yellow',
+              height: '25%',
+              boxShadow: 1,
+              width: 200,
+              marginLeft: 13,
+              fontSize: 15,
+            }}
+            style={{marginTop: '15px', marginBottom : '10px', display: 'block'}}
+            value={id}
+            onChange={onIdHandler}
+            label='ID'
+            />
+            <TextField 
+            sx={{
+              borderColor: 'yellow',
+              height: '25%',
+              boxShadow: 1,
+              width: 200,
+              marginLeft: 13,
+            }}
+            style={{marginBottom : '10px', display: 'block'}}
+            label='PASSWORD'
+            />
+     
+            <Button
+              sx = {{
+                bgcolor: '#FCF0D2',
+                fontcolor: '#324E66',
+                marginLeft: 13,
+                width: 200,
+              }}
+              > <Link to={'/manager/main'}>Login</Link>
+              </Button>
+    
+              <div className='FormControlLabel-wrapper'>
+              <FormControlLabel 
+                style={{ marginLeft: '90px'}} 
+                label="checkManagerLogin" 
+                
+              />
+              관리자 로그인
+              </div>
+              <div className='register-resetPassword'>
+                <div className='manager-register'>관리자 회원가입</div>
+                <div className='reset-pssword'>비밀번호 재설정</div>
+              </div>
+        </form>
+        {/* Form end */}
+      </div>
+    </div>
+
+  {/* Main화면 오른쪽 이미지 */}
+    <div className='main-right'>
+      <img className='main-bg-img' alt='#' src="img/Main-bg-img.png" />
+    </div>
+
+    </div>
+    </>
   )
 }
 
--- FE/sellerb_fe/src/pages/Manager/Main/ManagerMain.js
+++ FE/sellerb_fe/src/pages/Manager/Main/ManagerMain.js
@@ -1,42 +1,9 @@
-/*
- *
- *  관리자 Main
- *
- *  */
-
-import React from "react";
-import ManagerMainLeft from "../../../components/Manager/Main/ManagerMainLeft";
-import ManagerMainRight from "../../../components/Manager/Main/ManagerMainRignt";
+import React from 'react'
 
 function ManagerMain() {
-  const makeCenter = {
-    textAlign: "center",
-  };
-  const styleObj = {
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
-  };
-
   return (
-    <div style={makeCenter}>
-      {/* HeaderNavBar */}
-      <nav>
-        <div>HeaderNavBar</div>
-      </nav>
-      <div style={styleObj}>
-        {/* 좌측 환영인사 및 달력, 회의생성 */}
-
-        <ManagerMainLeft />
-
-        {/* 우측 상담사 관리 */}
-
-        <ManagerMainRight />
-      </div>
-      {/* footer */}
-      <div style={makeCenter}>footer</div>
-    </div>
-  );
+    <div>ManagerMain</div>
+  )
 }
 
-export default ManagerMain;
+export default ManagerMain
