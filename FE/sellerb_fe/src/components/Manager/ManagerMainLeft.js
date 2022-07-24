ba7219eaf37ddf85438d7d3d020195c1a651e77c
임채현 <dlacogus5239@google.com>
2022-07-24 19:11:20 +0900

    [S07P12D105-102] style : 상담사 Main 수정

---
+++ FE/sellerb_fe/src/components/Manager/ManagerMainLeft.js
@@ -0,0 +1,55 @@
+import React from "react";
+import isWeekend from "date-fns/isWeekend";
+import TextField from "@mui/material/TextField";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { StaticDatePicker } from "@mui/x-date-pickers";
+import Button from "@mui/material/Button";
+
+function ManagerMainLeft() {
+  const [value, setValue] = React.useState(new Date());
+  const styleObj_WrapDiv = {
+    width: "35%",
+  };
+  const styleObj_Button = {
+    textAlign: "center",
+    margin: "50px",
+  };
+  const welcome_text = {
+    display: "flex",
+    textAlign: "center",
+    size: "20px",
+    justifyContent: "center",
+  };
+  const blue = {
+    color: "blue",
+  };
+
+  return (
+    <div style={styleObj_WrapDiv}>
+      <div style={styleObj_Button}>ManagerMainLeft</div>
+      <div style={welcome_text}>
+        <h2 style={blue}>OOO</h2>
+        <h2>님 환영합니다!</h2>
+      </div>
+      {/* 달력 */}
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <StaticDatePicker
+          displayStaticWrapperAs='desktop'
+          openTo='day'
+          value={value}
+          shouldDisableDate={isWeekend}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      {/* 달력 END */}
+      <div style={styleObj_Button}>
+        <Button variant='contained'>회의 생성</Button>
+      </div>
+    </div>
+  );
+}
+export default ManagerMainLeft;
--- FE/sellerb_fe/src/pages/index.js
+++ FE/sellerb_fe/src/pages/index.js
@@ -1,3 +1,5 @@
-export { default as Main} from './Main/Main';
-export { default as ConsultantMain} from './Consultant/Main/ConsultantMain';
-export { default as ManagerMain} from './Manager/Main/ManagerMain';
+export { default as Main } from "./Main/Main";
+export { default as ConsultantMain } from "./Consultant/Main/ConsultantMain";
+export { default as ManagerMain } from "./Manager/Main/ManagerMain";
+export { default as ManagerMainLeft } from "../components/Manager/ManagerMainLeft";
+export { default as ManagerMainRight } from "../components/Manager/ManagerMainRight";
