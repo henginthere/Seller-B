e28adbf2335834c1899c0a9283670ffad3902796
DESKTOP-KVCQHCD\multicampus <dlacogus5239@gamil.com>
2022-07-25 16:54:28 +0900

    [-][FE] style: 관리자 MyPage 구성

--- FE/sellerb_fe/src/pages/index.js
+++ FE/sellerb_fe/src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Main } from "./Main/Main";
 export { default as ConsultantMain } from "./Consultant/Main/ConsultantMain";
 export { default as ManagerMain } from "./Manager/Main/ManagerMain";
-export {default as ManagerMyPage} from "./Manager/MyPage/ManagerMyPage"
+export { default as ManagerMyPage} from "./Manager/MyPage/ManagerMyPage"
