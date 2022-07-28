f75a5262e03cf2e14797d317c62c082706c8c8cf
henginthere <hypae@naver.com>
2022-07-28 09:38:45 +0900

    [S07P12D105-33] [BE] feat : 관리자 등록(회원가입) API

---
+++ BE/sellerB/src/main/java/backend/sellerB/controller/AuthController.java
@@ -0,0 +1,61 @@
+package backend.sellerB.controller;
+
+import backend.sellerB.dto.LoginDto;
+import backend.sellerB.dto.ManagerDto;
+import backend.sellerB.dto.TokenDto;
+import backend.sellerB.entity.Manager;
+import backend.sellerB.jwt.JwtFilter;
+import backend.sellerB.repository.ManagerRepository;
+import backend.sellerB.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final ManagerRepository managerRepository;
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+     //로그인을 했을때 토큰(AccessToken, RefreshToken)을 주는 메서드
+    @PostMapping("/authenticate")
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginDto loginDto) {
+        logger.info(loginDto.getId()+"###################");
+        TokenDto tokenDto = authService.authorize(loginDto.getId(), loginDto.getPass());
+        logger.info(loginDto.getId()+"#############");
+        return ResponseEntity.ok(tokenDto);
+    }
+
+    //AccessToken이 만료되었을 때 토큰(AccessToken , RefreshToken)재발급해주는 메서드
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue( @Valid @RequestBody TokenDto requestTokenDto) {
+        TokenDto tokenDto = authService
+                .reissue(requestTokenDto.getAccessToken(), requestTokenDto.getRefreshToken());
+        return ResponseEntity.ok(tokenDto);
+    }
+
+//    /*
+//       로그아웃을 했을 때 토큰을 받아 BlackList에 저장하는 메서드
+//     */
+//    @DeleteMapping("/authenticate")
+//    public ResponseEntity<Void> logout(
+//            @RequestBody @Valid TokenDto requestTokenDto) {
+//        authService.logout(requestTokenDto.getAccessToken(), requestTokenDto.getRefreshToken());
+//        return OK;
+//    }
+
+    @GetMapping("/test")
+    public String test(@Valid @RequestBody LoginDto loginDto) {
+//        logger.debug(loginDto.getId());
+//        console.log()
+
+        return loginDto.getId()+loginDto.getPass();
+    }
+
+}
