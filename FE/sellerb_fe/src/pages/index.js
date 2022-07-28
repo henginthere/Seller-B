779509fd532f2f402e99be263d7ee80940a9cf11
임채현 <dlacogus5239@google.com>
2022-07-28 11:56:16 +0900

    [S07P12D105-93][FE] feat : 상담사 마이페이지 UI

--- FE/sellerb_fe/src/pages/index.js
+++ FE/sellerb_fe/src/pages/index.js
@@ -1,11 +1,12 @@
 export { default as Main } from "./Main/Main";
 export { default as ConsultantMain } from "./Consultant/Main/ConsultantMain";
 export { default as ManagerMain } from "./Manager/Main/ManagerMain";
-export { default as ManagerMyPage} from "./Manager/MyPage/ManagerMyPage"
-export { default as ManageRegister } from '../components/Register/ManageRegister'
-export { default as NoticeList} from './Notice/NoticeList'
-export { default as NoticeWrite } from './Notice/NoticeWrite'
-export { default as NoticeDetail } from './Notice/NoticeDetail'
-export { default as ConsultantList } from './Manager/Consultant/ConsultantList'
-export { default as NoticeEdit } from './Notice/NoticeEdit'
-export { default as ConsultantDetail } from './Manager/Consultant/ConsultantDetail'
+export { default as ManagerMyPage } from "./Manager/MyPage/ManagerMyPage";
+export { default as ManageRegister } from "../components/Register/ManageRegister";
+export { default as NoticeList } from "./Notice/NoticeList";
+export { default as NoticeWrite } from "./Notice/NoticeWrite";
+export { default as NoticeDetail } from "./Notice/NoticeDetail";
+export { default as ConsultantList } from "./Manager/Consultant/ConsultantList";
+export { default as NoticeEdit } from "./Notice/NoticeEdit";
+export { default as ConsultantDetail } from "./Manager/Consultant/ConsultantDetail";
+export { default as ConsultantMyPage } from "./Consultant/MyPage/ConsultantMyPage";
