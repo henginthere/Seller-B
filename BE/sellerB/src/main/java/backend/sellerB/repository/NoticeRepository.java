db97ee554d40d145e938c406aee5530d4e0d100d
henginthere <hypae@naver.com>
2022-08-01 02:14:27 +0900

    [S07P12D105-40] [BE] feat : 게시글 제목검색 API 구현

--- BE/sellerB/src/main/java/backend/sellerB/repository/NoticeRepository.java
+++ BE/sellerB/src/main/java/backend/sellerB/repository/NoticeRepository.java
@@ -4,6 +4,9 @@ import backend.sellerB.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, Integer> {
+    List<Notice> findByNoticeTitleContaining(String noticeTitle);
 
 }
