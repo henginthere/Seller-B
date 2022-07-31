2201baeea1eeb5df1c725ae33507b0e187cb6721
henginthere <hypae@naver.com>
2022-07-31 20:57:58 +0900

    [S07P12D105-21] [BE] feat : RefreshToken 기능 추가

--- BE/sellerB/src/main/java/backend/sellerB/repository/ManagerRepository.java
+++ BE/sellerB/src/main/java/backend/sellerB/repository/ManagerRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface ManagerRepository extends JpaRepository<Manager, Integer> {
     @EntityGraph(attributePaths = "authorities") // EntityGraph annotation은 쿼리가 수행될때 Lazy조회가 아니고 Eager조회로 authorities 정보를 같이 가져옴
     Optional<Manager> findBymanagerId(String managerId);
+
+    Manager findByManagerSeq(int Seq);
 }
