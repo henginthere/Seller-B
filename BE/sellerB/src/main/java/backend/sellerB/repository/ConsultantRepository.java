35cde767bf1923444a0e7697c57b8c719b25e1e3
henginthere <hypae@naver.com>
2022-07-31 20:57:58 +0900

    [S07P12D105-21] [BE] feat : RefreshToken 기능 추가

--- BE/sellerB/src/main/java/backend/sellerB/repository/ConsultantRepository.java
+++ BE/sellerB/src/main/java/backend/sellerB/repository/ConsultantRepository.java
@@ -10,4 +10,6 @@ public interface ConsultantRepository extends JpaRepository<Consultant, Integer>
     @EntityGraph(attributePaths = "authorities") // EntityGraph annotation은 쿼리가 수행될때 Lazy조회가 아니고 Eager조회로 authorities 정보를 같이 가져옴
     Optional<Consultant> findByConsultantId(String consultantId);
 
+    Consultant findByConsultantSeq(int consultantSeq);
+
 }
