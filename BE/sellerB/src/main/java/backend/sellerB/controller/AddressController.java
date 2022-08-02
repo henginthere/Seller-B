b39397f31b35c7d1c2ebf502c6eb5efb3bb80adb
LJH <ljjuni1114@gmail.com>
2022-08-02 23:48:01 +0900

    [S07P12D105-196] [BE] feat : swagger 추가

--- BE/sellerB/src/main/java/backend/sellerB/controller/AddressController.java
+++ BE/sellerB/src/main/java/backend/sellerB/controller/AddressController.java
@@ -2,13 +2,21 @@ package backend.sellerB.controller;
 
 import backend.sellerB.dto.AddressDto;
 import backend.sellerB.service.AddressService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
-
+@Tag(name = "address", description = "배송지 API")
 @RestController
 @RequestMapping("/customer/address")
 public class AddressController {
@@ -18,6 +26,18 @@ public class AddressController {
         this.addressService = addressService;
     }
 
+//    @Operation(summary = "get posts", description = "지역에 대한 posts들 가져오기")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = PostsResponseDto.class))),
+//            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+//            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+//            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+//    })
+//    @Parameters({
+//            @Parameter(name = "province", description = "시", example = "경기도"),
+//            @Parameter(name = "city", description = "도", example = "고양시"),
+//            @Parameter(name = "hashtag", description = "검색한 해시태그", example = "['#자장면', '#중국집']")
+//    })
     @PostMapping
     public ResponseEntity<AddressDto> saveAddress(@Valid @RequestBody AddressDto addressDto) {
         return ResponseEntity.ok(addressService.create(addressDto));
