cab35fbb8a5b434d587250311ae545db5408794d
신혜연 <tgd_5753@naver.com>
2022-08-03 15:15:58 +0900

    [] [FE] : feat : noticeList.js Header <- AccessToken 추가

--- FE/sellerb_fe/src/api/api.js
+++ FE/sellerb_fe/src/api/api.js
@@ -1,18 +1,39 @@
 import axios from 'axios';
 import { getCookieToken } from '../storage/Cookie';
+import { useSelector } from 'react-redux';
 
-const cookieToken = getCookieToken();
+// 
+//export const RT = getCookieToken();
+// const AT = () =>
+//     useSelector((state)=> state.authToken.accessToken);
 
 // init base API 
-export const api = axios.create({
-    baseURL : "https://i7d105.p.ssafy.io/", 
+// axios.defaults.headers.common['Authorization'] = useSelector((state)=> state.authToken.accessToken);
+const apiInstance = axios.create({
+    baseURL : "https://i7d105.p.ssafy.io/api/", 
     headers : {
         "Content-Type" : `application/json`, 
-        Authorization: `Bearer ${
-            cookieToken 
-
-            // sessionStorage.getItem("access-token") ||
-            // localStorage.getItem("access-token")
-        }`,
     },
+});
+
+// export const getToken = ()=>{
+//     const a = useSelector((state)=>
+//         state.authToken.accessToken);
+// }
+
+export const api = apiInstance.interceptors.request.use(function (config) {
+    const token = useSelector((state)=>
+        state.authToken.accessToken
+    );
+
+    if(!token){
+        config.headers["accessToken"] = null;
+        config.headers["refreshToken"] = null;
+        return config;
+    }
+    if(config.headers && token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+
+        return config; 
+    }
 });
