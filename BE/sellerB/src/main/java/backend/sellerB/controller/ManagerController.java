892680a7f67bb4d81d10be0e018ab3ea32ecc487
henginthere <hypae@naver.com>
2022-08-04 02:42:48 +0900

    [S07P12D105-201] [BE] 관리자 삭제 API 구현

--- BE/sellerB/src/main/java/backend/sellerB/controller/ManagerController.java
+++ BE/sellerB/src/main/java/backend/sellerB/controller/ManagerController.java
@@ -1,5 +1,6 @@
 package backend.sellerB.controller;
 
+import backend.sellerB.dto.ConsultantDto;
 import backend.sellerB.dto.ManagerDto;
 import backend.sellerB.dto.NoticeDto;
 import backend.sellerB.service.AuthService;
@@ -8,10 +9,7 @@ import backend.sellerB.service.NoticeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -30,6 +28,12 @@ public class ManagerController {
     @PostMapping("/register")
     public ResponseEntity<ManagerDto> registerManager(@Valid @RequestBody ManagerDto managerDto) {
         return ResponseEntity.ok(managerService.signup(managerDto));
+    }
+
+    @DeleteMapping("/{managerSeq}")
+    public ResponseEntity<ManagerDto> deleteManager(@PathVariable Integer managerSeq) {
+        // Access the DB and delete the order
+        return ResponseEntity.ok(managerService.delete(managerSeq));
     }
 
 
