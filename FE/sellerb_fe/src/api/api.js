f8e65ca54796ed5f3f1c72d0febc7696aa78c8f2
MilanoBeer <tgd5753@gmail.com>
2022-08-03 10:29:35 +0900

    [] [FE] : feat : login API Test

--- FE/sellerb_fe/src/api/api.js
+++ FE/sellerb_fe/src/api/api.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
+import { getCookieToken } from '../storage/Cookie';
+
+const cookieToken = getCookieToken();
 
 // init base API 
 export const api = axios.create({
     baseURL : "https://i7d105.p.ssafy.io/", 
     headers : {
         "Content-Type" : `application/json`, 
-        // Authorization: `Bearer ${
-        //     sessionStorage.getItem("access-token") ||
-        //     localStorage.getItem("access-token")
-        // }`,
+        Authorization: `Bearer ${
+            cookieToken 
+
+            // sessionStorage.getItem("access-token") ||
+            // localStorage.getItem("access-token")
+        }`,
     },
 });
