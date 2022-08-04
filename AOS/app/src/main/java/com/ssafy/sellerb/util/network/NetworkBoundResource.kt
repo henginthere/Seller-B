d605cc38eb0d90cc91f24683f3b8b6d798c09257
Gideok <76939190+GideokLee@users.noreply.github.com>
2022-08-04 23:41:11 +0900

    [S07P12D105-23][AOS] feat : Network util 구현

---
+++ AOS/app/src/main/java/com/ssafy/sellerb/util/network/NetworkBoundResource.kt
@@ -0,0 +1,44 @@
+package com.ssafy.sellerb.util.network
+
+
+import com.ssafy.sellerb.util.Resource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.*
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType?>,
+    crossinline fetch: suspend () -> RequestType,
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,
+    crossinline shouldFetch: (ResultType?) -> Boolean,
+    coroutineDispatcher: CoroutineDispatcher
+) = flow<Resource<ResultType>> {
+
+    // check for data in database
+    val data = query().firstOrNull()
+
+    if (data != null) {
+        // data is not null -> update loading status
+        emit(Resource.loading(data))
+    }
+
+    if (shouldFetch(data)) {
+        // Need to fetch data -> call backend
+        val fetchResult = fetch()
+        // got data from backend, store it in database
+        saveFetchResult(fetchResult)
+    }
+
+    // load updated data from database (must not return null anymore)
+    val updatedData = query().first()
+
+    // emit updated data
+    emit(Resource.success(updatedData))
+
+}.onStart {
+    emit(Resource.loading(null))
+
+}.catch { exception ->
+
+    emit(Resource.error(null))
+
+}.flowOn(coroutineDispatcher)
