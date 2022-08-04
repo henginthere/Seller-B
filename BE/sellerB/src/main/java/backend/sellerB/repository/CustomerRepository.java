0db99dae1a73b3117febb94153176d351306a5bc
henginthere <hypae@naver.com>
2022-08-04 17:39:08 +0900

    [BE] fix : entity 수정

--- BE/sellerB/src/main/java/backend/sellerB/repository/CustomerRepository.java
+++ BE/sellerB/src/main/java/backend/sellerB/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package backend.sellerB.repository;
 import backend.sellerB.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
