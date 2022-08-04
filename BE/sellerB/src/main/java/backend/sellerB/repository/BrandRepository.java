35fee3d623b493d6526d69eaff86b222c1e7e8a3
LJH <ljjuni1114@gmail.com>
2022-08-04 23:19:02 +0900

    [BE] fix : 제품군 dto 수정

--- BE/sellerB/src/main/java/backend/sellerB/repository/BrandRepository.java
+++ BE/sellerB/src/main/java/backend/sellerB/repository/BrandRepository.java
@@ -3,5 +3,8 @@ package backend.sellerB.repository;
 import backend.sellerB.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    Optional<Brand> findByBrandNameKor(String brandName);
 }
