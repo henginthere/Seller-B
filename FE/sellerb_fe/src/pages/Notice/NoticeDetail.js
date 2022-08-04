1452a93a298ad157e5c866fd9b2c2c4ca82d5432
임채현 <dlacogus5239@google.com>
2022-08-04 18:18:47 +0900

    [S07P12D105-200][FE] feature: 공지사항 CRU 완료

--- FE/sellerb_fe/src/pages/Notice/NoticeDetail.js
+++ FE/sellerb_fe/src/pages/Notice/NoticeDetail.js
@@ -3,10 +3,12 @@ import styled from "styled-components";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import "./NoticeWrite.css";
 import { Footer, NavBar } from "../../components/index";
-// api 
-import { detailNoticeApi } from "../../api/noticeApi";
-
-import { getStringDate } from "../../utils/date";
+// api
+import {
+  detailNoticeApi,
+  modifyNoticeApi,
+  delNoticeApi,
+} from "../../api/noticeApi";
 
 const NoticeWrapper = styled.div`
   padding: 3rem 5rem;
@@ -14,64 +16,154 @@ const NoticeWrapper = styled.div`
   width: 100%;
   overflow: auto;
 `;
+var isManager = true;
 
 const NoticeDetail = () => {
   const params = useParams();
- 
-  console.log(params.notice_seq);
+
+  console.log("params noticeSeq : " + params.notice_seq);
 
   const navigate = useNavigate();
-
-  const [readOnly, setReadOnly] = useState(true)
   const [noticeData, setNoticeData] = useState({
-    title: "",
-    content: "", 
-  })
+    noticeSeq: "",
+    noticeTitle: "",
+    noticeContent: "",
+  });
+  var { noticeSeq, noticeTitle, noticeContent } = noticeData;
+  const [isModify, setIsModify] = useState(false);
 
   // const dispatch = useDispatch();
+  const handleChange = (e) => {
+    e.preventDefault();
+    // console.log("handleChange!");
 
-// mount시점에서, 공지사항 id에 해당하는 게시글 받아오기 
-  useEffect(()=>{
+    noticeData[e.target.name] = e.target.value;
+    console.log("==handleChange== ");
+    console.log(noticeData);
+  };
+  // mount시점에서, 공지사항 id에 해당하는 게시글 받아오기
+  useEffect(() => {
     detailNoticeApi(params.id)
-    .then((res)=>{
+      .then((res) => {
+        // console.log("== response Data in useEffect == ");
+        // console.log(res.data);
         // response에 대해 비구조화 할당
         setNoticeData(res.data);
-        console.log(noticeData);
-    })
-    .catch((e)=>{
-        console.log("err")
-    })
-  }, [])
+
+        // console.log("== noticeData in useEffect ==");
+        // console.log(noticeData);
+      })
+      .catch((e) => {
+        console.log("err");
+      });
+  }, []);
+
+  const delNotice = () => {
+    if (window.confirm("삭제하시겠습니까?")) {
+      console.log(
+        "noticeData.noticeSeq in delNotice : " + noticeData.noticeSeq,
+      );
+      delNoticeApi(noticeData.noticeSeq);
+      navigate("/manager/noticeList");
+    } else {
+      return;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert("수정하시겠습니까?");
+    console.log("== noticeData in handleSubmit ==");
+    console.log(noticeData);
+    setNoticeData({
+      ...noticeData,
+    });
+    console.log("=== FINAL notice Data ===");
+    console.log(noticeData);
+    modifyNoticeApi(noticeData);
+    setIsModify(!isModify);
+  };
+  const ModifyNotice = () => {
+    return (
+      <form className='write-form-wrapper' onSubmit={handleSubmit}>
+        <div className='write-input-title'>
+          <input
+            className='write-title'
+            defaultValue={noticeData.noticeTitle}
+            onChange={handleChange}
+            name='noticeTitle'
+          />
+        </div>
+        <div className='write-input-content'>
+          <input
+            className='write-content'
+            defaultValue={noticeData.noticeContent}
+            onChange={handleChange}
+            name='noticeContent'
+          />
+        </div>
+        <div>
+          <div>
+            <button type='submit'>수정</button>
+            <button
+              onClick={() => {
+                setIsModify(!isModify);
+                // console.log("After : " + isModify);
+              }}
+            >
+              취소
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  };
+  const DefaultNotice = () => {
+    return (
+      <div className='write-form-wrapper'>
+        <div className='write-input-title'>
+          <input
+            readOnly={true}
+            className='write-title'
+            defaultValue={noticeData.noticeTitle}
+          />
+        </div>
+        <div className='write-input-content'>
+          <input
+            readOnly={true}
+            className='write-content'
+            defaultValue={noticeData.noticeContent}
+          />
+        </div>
+        <div>
+          <Link to='/manager/noticeList'>
+            <button>뒤로</button>
+          </Link>
+          {isManager ? (
+            <div>
+              <button
+                onClick={() => {
+                  setIsModify(!isModify);
+                  // console.log(isModify);
+                }}
+              >
+                수정
+              </button>
+              <button onClick={delNotice}>삭제</button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    );
+  };
 
   return (
     <>
       <NavBar />
       <NoticeWrapper>
-        <div className="notice-write-header">공지사항 내용</div>
-        <div className="write-form-wrapper">
-          <div className="write-input-title">
-            <input
-              readOnly= {readOnly}
-              className="write-title"
-              placeholder="DetailPage : 수정불가"
-            />
-          </div>
-          <div className="write-input-content">
-            <input
-              readOnly={readOnly}
-              className="write-content"
-              placeholder="DetailPage : 수정불가"
-            />
-          </div>
-          <div>
-            <Link to="/manager/noticeList">
-              <button>뒤로</button>
-            </Link>
-            <Link to="/manager/noticeEdit">
-            <button>수정</button>
-            </Link>
-          </div>
-        </div>
+        <div className='notice-write-header'>공지사항 내용</div>
+        {isModify ? <ModifyNotice /> : <DefaultNotice />}
       </NoticeWrapper>
       <Footer />
     </>
