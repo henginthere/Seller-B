bb2eb6cacb3e184e5f587f0d678fba8cf8f40524
임채현 <dlacogus5239@google.com>
2022-08-06 00:33:49 +0900

    [-][FE] style : package.json 수정

--- FE/sellerb_fe/src/App.js
+++ FE/sellerb_fe/src/App.js
@@ -29,43 +29,37 @@ import {
 function App() {
   return (
     <>
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/manager/main' element={<ManagerMain />} />
-          <Route path='/consultant/main' element={<ConsultantMain />} />
-          <Route path='/manager/mypage' element={<ManagerMyPage />} />
-          <Route path='/manager/register' element={<ManageRegister />} />
-          <Route path='/manager/noticeList' element={<NoticeList />} />
-          <Route path='/manager/noticeWrite' element={<NoticeWrite />} />
-          <Route path='/noticeDetail/:id' element={<NoticeDetail />} />
-          <Route path='/manager/noticeEdit' element={<NoticeEdit />} />
-          <Route path='/manager/consultantList' element={<ConsultantList />} />
-          <Route
-            path='/manager/consultantDetail/:id'
-            element={<ConsultantDetail />}
-          />
-          <Route
-            path='/manager/consultantRegister'
-            element={<ConsultantRegister />}
-          />
-          <Route
-            path='/manager/consultantModify/:id'
-            element={<ConsultantModify />}
-          />
-          <Route path='/consultant/mypage' element={<ConsultantMyPage />} />
-          <Route path='/manager/productList' element={<ProductList />} />
-          <Route
-            path='/manager/productRegister'
-            element={<ProductRegister />}
-          />
-          <Route
-            path='/manager/productDetail/:seq'
-            element={<ProductDetail />}
-          />
-          <Route path='/manager/productEdit/:seq' element={<ProductEdit />} />
-          <Route path='/manager/waitingPage/:seq' element={<WaitingPage />} />
-	  {/* <Route path='/meeting/mancon' element={<MeetingManCon />} /> */}
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/manager/main' element={<ManagerMain />} />
+        <Route path='/consultant/main' element={<ConsultantMain />} />
+        <Route path='/manager/mypage' element={<ManagerMyPage />} />
+        <Route path='/manager/register' element={<ManageRegister />} />
+        <Route path='/manager/noticeList' element={<NoticeList />} />
+        <Route path='/manager/noticeWrite' element={<NoticeWrite />} />
+        <Route path='/noticeDetail/:id' element={<NoticeDetail />} />
+        <Route path='/manager/noticeEdit' element={<NoticeEdit />} />
+        <Route path='/manager/consultantList' element={<ConsultantList />} />
+        <Route
+          path='/manager/consultantDetail/:id'
+          element={<ConsultantDetail />}
+        />
+        <Route
+          path='/manager/consultantRegister'
+          element={<ConsultantRegister />}
+        />
+        <Route
+          path='/manager/consultantModify/:id'
+          element={<ConsultantModify />}
+        />
+        <Route path='/consultant/mypage' element={<ConsultantMyPage />} />
+        <Route path='/manager/productList' element={<ProductList />} />
+        <Route path='/manager/productRegister' element={<ProductRegister />} />
+        <Route path='/manager/productDetail/:seq' element={<ProductDetail />} />
+        <Route path='/manager/productEdit/:seq' element={<ProductEdit />} />
+        <Route path='/manager/waitingPage/:seq' element={<WaitingPage />} />
+        <Route path='/meeting/mancon' element={<MeetingManCon />} />
+      </Routes>
     </>
   );
 }
