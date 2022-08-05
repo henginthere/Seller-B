3114f35e30e425811512e4f0b2e8dcf6202012e2
henginthere <hypae@naver.com>
2022-08-06 01:06:51 +0900

    [S07P12D105-204] [BE] fix: 로그인 response 수정

--- BE/sellerB/src/main/java/backend/sellerB/dto/LoginResponseDto.java
+++ BE/sellerB/src/main/java/backend/sellerB/dto/LoginResponseDto.java
@@ -11,11 +11,13 @@ public class LoginResponseDto {
     private TokenDto tokenDto;
     private String authority;
     private Long seq;
+    private Long brandSeq;
 
     @Builder
-    public LoginResponseDto(TokenDto tokenDto, String authority, Long seq) {
+    public LoginResponseDto(TokenDto tokenDto, String authority, Long seq, Long brandSeq) {
         this.tokenDto = tokenDto;
         this.authority = authority;
         this.seq = seq;
+        this.brandSeq = brandSeq;
     }
 }
