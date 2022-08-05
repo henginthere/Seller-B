2723a7445e4d4c10ef07df06b727d04dacdff3e0
MilanoBeer <tgd5753@gmail.com>
2022-08-05 14:48:45 +0900

    [S07P12D105-210] [FE] : feat : 상담사 정보 수정 API 연결중

--- FE/sellerb_fe/src/pages/Manager/Main/ManagerMain.js
+++ FE/sellerb_fe/src/pages/Manager/Main/ManagerMain.js
@@ -3,15 +3,16 @@
  *  관리자 Main
  *
  *  */
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ManagerMainLeft from "../../../components/Manager/Main/ManagerMainLeft";
 import ManagerMainRight from "../../../components/Manager/Main/ManagerMainRight";
+import ManagerMainCenter from "../../../components/Manager/Main/ManagerMainCenter";
 import NavBar from "../../../components/Common/NavBar/NavBar";
 import Footer from "../../../components/Common/Footer/Footer";
-import { listConsultantApi } from '../../../api/consultantApi'
+import { listConsultantApi } from "../../../api/consultantApi";
+import './ManagerMain.css';
 
 function ManagerMain() {
-  
   // const [list, setList] = useState([]);
 
   // useEffect(()=>{
@@ -21,12 +22,11 @@ function ManagerMain() {
   //     setList(res.data);
 
   //     console.log("consultantList: " + list[0].consultantId)
-  //   })  
+  //   })
   //   .catch((err)=>{
   //     console.log("err:" + err.data);
   //   })
   // },[])
-  
 
   return (
     <div className="center">
@@ -36,12 +36,16 @@ function ManagerMain() {
       </nav>
       <div id="wrapper">
         {/* 좌측 환영인사 및 달력, 회의생성 */}
-
-        <ManagerMainLeft />
-
+        <div id="manager-main-left">
+          <ManagerMainLeft />
+        </div>
         {/* 우측 상담사 관리 */}
-
-        <ManagerMainRight />
+        <div id="manager-main-right">
+          <ManagerMainRight />
+        </div>
+        <div id="manager-main-center">
+          <ManagerMainCenter />
+        </div>
       </div>
       {/* footer */}
       <div className="center">
