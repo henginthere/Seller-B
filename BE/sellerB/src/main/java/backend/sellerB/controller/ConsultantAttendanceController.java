75049b635d904d8b7496e386e3564deb5dbabcc1
LJH <ljjuni1114@gmail.com>
2022-08-05 23:18:23 +0900

    [S07P12D105-129] [BE] feat : 상담사 출결이력 API

---
+++ BE/sellerB/src/main/java/backend/sellerB/controller/ConsultantAttendanceController.java
@@ -0,0 +1,43 @@
+package backend.sellerB.controller;
+
+import backend.sellerB.dto.BrandDto;
+import backend.sellerB.dto.ConsultantAttendanceDto;
+import backend.sellerB.dto.ResponseBrandDto;
+import backend.sellerB.service.ConsultantAttendanceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/brand")
+public class ConsultantAttendanceController {
+    private final ConsultantAttendanceService consultantAttendanceService;
+
+    public ConsultantAttendanceController(ConsultantAttendanceService consultantAttendanceService) {
+        this.consultantAttendanceService = consultantAttendanceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ConsultantAttendanceDto> saveConsultantAttendanc(@Valid @RequestBody ConsultantAttendanceDto consultantAttendanceDto) {
+        return ResponseEntity.ok(consultantAttendanceService.create(consultantAttendanceDto));
+    }
+
+
+    @GetMapping("/list")
+    public ResponseEntity<List<ConsultantAttendanceDto>> getConsultantAttendancList(HttpServletRequest request) {
+        return ResponseEntity.ok(consultantAttendanceService.getConsultantAttendanceList());
+    }
+
+    @GetMapping("/list/{consultant-id}")
+    public ResponseEntity<List<ConsultantAttendanceDto>> getConsultantAttendancListByConsultantId(HttpServletRequest request,  @PathVariable("consultant-id") String consultantId) {
+        return ResponseEntity.ok(consultantAttendanceService.getConsultantAttendanceListByConsultantId(consultantId));
+    }
+
+    @PutMapping("/{seq}")
+    public ResponseEntity<ConsultantAttendanceDto> updateConsultantAttendance(@Valid @RequestBody ConsultantAttendanceDto consultantAttendanceDto, @PathVariable Long seq) {
+        return ResponseEntity.ok(consultantAttendanceService.updateConsultantAttendance(seq, consultantAttendanceDto));
+    }
+}
