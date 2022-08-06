bf514696582fab96c5d6ba84e336222867e30eea
MilanoBeer <tgd5753@gmail.com>
2022-08-06 23:14:35 +0900

    [S07P12D105-218] [FE] : feat : 제품 등록 api 연결

--- FE/sellerb_fe/src/pages/Manager/Product/ProductList.js
+++ FE/sellerb_fe/src/pages/Manager/Product/ProductList.js
@@ -1,102 +1,97 @@
-import React, { useState , useEffect} from 'react'
-import { Link, useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import Axios from 'axios'
-import "./ProductList.css"
+import Axios from "axios";
+import "./ProductList.css";
 import { Footer, NavBar, ProdcutOption } from "../../../components/index";
-import { productLineListApi} from '../../../api/productApi'
+import { productGroupListApi } from "../../../api/productApi";
 
 function ProductList() {
-   
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [optionList, setOptionList] = useState(["선풍기", "김치냉장고"]);
-    const [searchWord, setSearchWord] = useState("");
-    const [groupOption, setGroupOption] = useState("");
+  const [groupList, setGroupList] = useState([]);
+  const [managerBrand, setManagerBrand] = useState(
+    localStorage.getItem("brandNameKor")
+  );
 
-    const onHandleGroupOption = (event)=>{
-        setGroupOption(event.currentTarget.value);
-    
-        // console.log(groupOption);
-      }
+  const [searchWord, setSearchWord] = useState("");
+  const [groupOption, setGroupOption] = useState("");
 
-    const onSearchWordHandler = (e)=>{
-        setSearchWord(e.target.value);
-        console.log(searchWord)
-    }
+  const onHandleGroupOption = (event) => {
+    setGroupOption(event.currentTarget.value);
 
-    const onSearchBtn = () => {
-        //
-    }
+    // console.log(groupOption);
+  };
 
-    const onRegisterBtn = ()=>{
-        navigate('/manager/productRegister')
-    }
+  const onSearchWordHandler = (e) => {
+    setSearchWord(e.target.value);
+    console.log(searchWord);
+  };
 
-    // 선택한 제품군 option에 따라, 나타낼 해당 제품군 리스트 컴포넌트
-    function GroupOptionList( ){
-        return (
-            <ProdcutOption group = {groupOption} />
-        );
-    }
+  const onSearchBtn = () => {
+    //
+  };
 
-    // useEffect(() => {
-    //     productLineListApi()
-    //       .then((res) => {
-    //         console.log(res.data);
-    //         setOptionList(res.data);
+  const onRegisterBtn = () => {
+    navigate("/manager/productRegister");
+  };
 
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //       });
-    //   }, []);
+  // 선택한 제품군 option에 따라, 나타낼 해당 제품군 리스트 컴포넌트
+  function GroupOptionList() {
+    return <ProdcutOption group={groupOption} />;
+  }
+
+  useEffect(() => {
+    productGroupListApi()
+      .then((res) => {
+        setGroupList(res.data); // groupList
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
-        <NavBar />
-            <div className="page-title">제품 목록</div>
-            <div className="product-list">
-                <div className='page-navi-wrapper'>
-                    <div className='navi-left'>
-                        <select onChange={onHandleGroupOption} value={groupOption}>
-                            {
-                                optionList.map((ele, i)=>{  
-                                    return (
-                                    <option>{ele}</option>
-                                    )
-                                })
-                            }
-                            <option>에어컨</option>
-                            <option>냉장고</option>
-                            <option>전자레인지</option>
-                        </select>
-                    </div>
-                    <div className='navi-right'>
-                        <input 
-                            placeholder='제품검색하기'  
-                            value={searchWord}
-                            onChange={onSearchWordHandler}
-                        />
-                        <button>go</button>
-                        
-                        <Link to="/manager/productRegister">
-                        <div >
-                            제품등록
-                        </div>
-                        </Link>
+      <NavBar />
+      <div className="page-title">제품 목록</div>
+      <div className="product-list">
+        <div className="page-navi-wrapper">
+          <div className="navi-left">
+            <select onChange={onHandleGroupOption} value={groupOption}>
+                {groupList.map((option) =>
+                  option.brandName === managerBrand ? (
+                    <option>{option.productGroupName}</option>
+                  ) : (
+                    ""
+                  )
+                )}
+              <option>에어컨</option>
+              <option>냉장고</option>
+              <option>전자레인지</option>
+            </select>
+          </div>
+          <div className="navi-right">
+            <input
+              placeholder="제품검색하기"
+              value={searchWord}
+              onChange={onSearchWordHandler}
+            />
+            <button>go</button>
 
-                    </div>
-                </div>
-                <div className='product-list-wrapper'>
-                    <GroupOptionList />
-                </div>
-                {/* end : page-navi-wrapper */}
-            </div>
-        <Footer />
+            <Link to="/manager/productRegister">
+              <div>제품등록</div>
+            </Link>
+          </div>
+        </div>
+        <div className="product-list-wrapper">
+          <GroupOptionList />
+        </div>
+        {/* end : page-navi-wrapper */}
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-
-export default ProductList
+export default ProductList;
