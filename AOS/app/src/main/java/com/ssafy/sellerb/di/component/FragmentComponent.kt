c84f3a42bf1146f131de71404200369a0668d82f
Gideok <76939190+GideokLee@users.noreply.github.com>
2022-08-08 10:54:21 +0900

    [S07P12D105-85] [AOS] feat : 상담대기화면 구현

--- AOS/app/src/main/java/com/ssafy/sellerb/di/component/FragmentComponent.kt
+++ AOS/app/src/main/java/com/ssafy/sellerb/di/component/FragmentComponent.kt
@@ -2,6 +2,7 @@ package com.ssafy.sellerb.di.component
 
 import com.ssafy.sellerb.FragmentScope
 import com.ssafy.sellerb.di.module.FragmentModule
+import com.ssafy.sellerb.ui.consulting.WaitingFragment
 import com.ssafy.sellerb.ui.home.HomeFragment
 import com.ssafy.sellerb.ui.login.LoginFragment
 import com.ssafy.sellerb.ui.mypage.MyPageFragment
@@ -17,5 +18,6 @@ interface FragmentComponent {
     fun inject(fragment: HomeFragment)
     fun inject(fragment: MyPageFragment)
     fun inject(fragment: LoginFragment)
+    fun inject(fragment: WaitingFragment)
 
 }
