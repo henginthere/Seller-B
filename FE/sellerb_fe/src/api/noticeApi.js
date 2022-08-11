fcecac02e4652af319fd3730a0514661502838c2
신혜연 <tgd_5753@naver.com>
2022-08-11 14:33:55 +0900

    [][FE] fix : axios 객체 수정

--- FE/sellerb_fe/src/api/noticeApi.js
+++ FE/sellerb_fe/src/api/noticeApi.js
@@ -1,4 +1,4 @@
-import { api } from "./api";
+import api from "./api";
 // import { useSelector } from 'react-redux'
 
 // post : Register : NoticeWrite.js
--- FE/sellerb_fe/src/api/productApi.js
+++ FE/sellerb_fe/src/api/productApi.js
@@ -1,12 +1,15 @@
-import { api } from './api';
+import api from './api';
 
 // POST : 제품 등록
 export const productRegisterApi = async (product, success, fail) => {
+    
+    console.log("in Register API : " + JSON.stringify(product));
     return await api.post("/product", product).then(success).catch(fail);
 }
 
 // GET : List : ProductList.js : 모든 제품군 '이름'을 가져옴
 export const productGroupListApi = async (success, fail) =>{
+    console.log("api 객체 : " + api)
     return await api.get("/product-group/list").then(success).catch(fail);
 }
 
