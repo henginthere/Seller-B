db395f587708025c9b1ecdb62aff264112606f64
henginthere <hypae@naver.com>
2022-08-12 16:05:12 +0900

    [] [BE] feat : 상담후기 조회(상담사별, 고객별) API 구현

--- BE/sellerB/src/main/java/backend/sellerB/service/ReviewService.java
+++ BE/sellerB/src/main/java/backend/sellerB/service/ReviewService.java
@@ -31,12 +31,17 @@ public class ReviewService {
         return ReviewDto.from(reviewRepository.save(review));
     }
 
-    public List<ReviewDto> getReviewListByConsultantId(String consultantId) {
-
-        return ReviewDto.fromList(reviewRepository.findAll());}
+    public List<ReviewDto> getReviewListByConsultantSeq(Long consultantSeq) {
+        Optional<List<Review>> optionalReviewList = reviewRepository.findByConsulting_Consultant_ConsultantSeq(consultantSeq);
+        List<Review> reviewList = optionalReviewList.get();
+        return ReviewDto.fromList(reviewList);
+    }
 
     public List<ReviewDto> getReviewListByCustomerSeq(Long customerSeq) {
-        return ReviewDto.fromList(reviewRepository.findAll());}
+        Optional<List<Review>> optionalReviewList = reviewRepository.findByConsulting_Customer_CustomerSeq(customerSeq);
+        List<Review> reviewList = optionalReviewList.get();
+        return ReviewDto.fromList(reviewList);
+    }
 
     public ReviewDto getReviewDetail(Long seq) {
         Optional<Review> reviewOptional = reviewRepository.findById(seq);
