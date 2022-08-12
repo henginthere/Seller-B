41deadc3779771b9edff2620a77f3bbfa7c65d19
DESKTOP-KVCQHCD\multicampus <yeonbb98@naver.com>
2022-08-12 16:05:12 +0900

    [] [BE] feat : 상담후기 조회(상담사별, 고객별) API 구현

--- BE/sellerB/src/main/java/backend/sellerB/repository/ReviewRepository.java
+++ BE/sellerB/src/main/java/backend/sellerB/repository/ReviewRepository.java
@@ -3,5 +3,11 @@ package backend.sellerB.repository;
 import backend.sellerB.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Optional<List<Review>> findByConsulting_Consultant_ConsultantSeq(Long seq);
+    Optional<List<Review>> findByConsulting_Customer_CustomerSeq(Long seq);
 }
