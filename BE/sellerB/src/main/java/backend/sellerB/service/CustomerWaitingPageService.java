914be2b275fd270a6b4709286763828f6987c70d
henginthere <hypae@naver.com>
2022-08-13 00:50:10 +0900

    [] [BE] feat : waiting page 제품별 하나만 등록할 수 있게 처리 (중복 시 403에러코드?)

--- BE/sellerB/src/main/java/backend/sellerB/service/CustomerWaitingPageService.java
+++ BE/sellerB/src/main/java/backend/sellerB/service/CustomerWaitingPageService.java
@@ -4,12 +4,16 @@ import backend.sellerB.dto.CustomerWaitingPageDto;
 import backend.sellerB.dto.RegisterCustomerWaitingPageDto;
 import backend.sellerB.entity.CustomerWaitingPage;
 import backend.sellerB.entity.Product;
+import backend.sellerB.exception.DuplicateWaitingPageException;
 import backend.sellerB.repository.CustomerWaitingPageRepository;
 import backend.sellerB.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.PreUpdate;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -22,19 +26,27 @@ public class CustomerWaitingPageService {
     private final ProductRepository productRepository;
 
 //    private final AwsS3Service awsS3Service;
+    @PreUpdate
     public CustomerWaitingPageDto create(RegisterCustomerWaitingPageDto registerCustomerWaitingPageDto) throws IOException {
 
+        Optional<CustomerWaitingPage> customerWaitingPageOptional = customerWaitingPageRepository.findByProduct_ProductSeq(registerCustomerWaitingPageDto.getProductSeq());
+        if(!(customerWaitingPageOptional.isPresent())) {
 //        String customerWaitingPageImage = awsS3Service.upload(registerCustomerWaitingPageDto.getCustomerWaitingPageImageFile(), "static");
-        Optional<Product> productOptional = productRepository.findById(registerCustomerWaitingPageDto.getProductSeq());
-        Product product = productOptional.get();
+            Optional<Product> productOptional = productRepository.findById(registerCustomerWaitingPageDto.getProductSeq());
+            Product product = productOptional.get();
 
-        CustomerWaitingPage customerWaitingPage = CustomerWaitingPage.builder()
-                .product(product)
-                .customerWaitingPageMent(registerCustomerWaitingPageDto.getCustomerWaitingPageMent())
-                .customerWaitingPageImage(registerCustomerWaitingPageDto.getCustomerWaitingPageImageUrl())
-                .build();
+            CustomerWaitingPage customerWaitingPage = CustomerWaitingPage.builder()
+                    .product(product)
+                    .customerWaitingPageMent(registerCustomerWaitingPageDto.getCustomerWaitingPageMent())
+                    .customerWaitingPageImage(registerCustomerWaitingPageDto.getCustomerWaitingPageImageUrl())
+                    .build();
 
-        return CustomerWaitingPageDto.from(customerWaitingPageRepository.save(customerWaitingPage));
+            return CustomerWaitingPageDto.from(customerWaitingPageRepository.save(customerWaitingPage));
+        }
+        else{
+            return null;
+        }
+
     }
 
     public List<CustomerWaitingPageDto> getCustomerWaitingPageList() { return CustomerWaitingPageDto.fromList(customerWaitingPageRepository.findAll());}
