c583f7fa49d7c9205ec69b13f80c58f5fc3fd23f
신혜연 <tgd_5753@naver.com>
2022-08-12 21:45:16 +0900

    [][FE] : styled : NavBar.js 링크 수정, 아이콘 추가 & SmallButton,MediButton hover 추가

--- FE/sellerb_fe/src/components/Common/NavBar/NavBar.js
+++ FE/sellerb_fe/src/components/Common/NavBar/NavBar.js
@@ -3,6 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 import { getManagerInfoApi } from "../../../api/managerApi";
 import { detailConsultantApi } from "../../../api/consultantApi";
+import styled from 'styled-components'
+import 
+  { NotificationOutlined, YoutubeOutlined, 
+    DatabaseOutlined, LogoutOutlined,
+    BuildOutlined} from "@ant-design/icons";
+import { CgProfile } from "react-icons/cg";
+
+
+
 
 function NavBar() {
   const navigate = useNavigate();
@@ -16,59 +25,93 @@ function NavBar() {
   }
 
   // 관리자, 상담사든 로그인할 때, 사용자의 seq받기
-  useEffect(()=>{
+  useEffect(() => {
     const seq = sessionStorage.getItem("seq");
     setSeq(seq);
-    console.log("받은 seq : " + seq)
-  }, [])
+    console.log("받은 seq : " + seq);
+  }, []);
+
+  const onLogOutBtn = () => {
+    sessionStorage.removeItem("accessToken");
+  };
 
   return (
     <>
-      <div className='navbar-wrapper'>
-        <div className='navbar-left'>
-          <Link to={isManager ? "/manager/main" : "/consultant/main"} style={{ marginTop: "7px" }}>
+      <div className="navbar-wrapper">
+        <div className="navbar-left">
+          <Link
+            to={isManager ? "/manager/main" : "/consultant/main"}
+            style={{ marginTop: "7px" }}
+          >
             <img
-              className='navbar-logo-img'
-              alt='#'
+              className="navbar-logo-img"
+              alt="#"
               src={`${process.env.PUBLIC_URL}/img/sellerB_Logo.svg`}
             />
           </Link>
-          <div className='small-sellerb'>SellerB</div>
+          <div className="small-sellerb">SellerB</div>
         </div>
 
-        <div className='navbar-right'>
+        <div className="navbar-right">
           {isManager ? (
-            <div className='navbar-right-navigator'>
-              <Link to='/manager/noticeList' className='link-to'>
-                <h4>공지사항</h4>
-              </Link>
-              <Link to='/meeting/mancon' className='link-to'>
-                <h4>회의 생성</h4>
-              </Link>
-              <Link to='/manager/productList' className='link-to'>
-                <h4>제품관리</h4>
-              </Link>
-              <div className='link-to' onClick={()=> navigate(`/manager/mypage/${seq}`)}>
-                <h4>마이페이지</h4>
+            <div className="navbar-right-navigator">
+              <div
+                className="navi-title"
+                onClick={() => navigate("/manager/noticeList")}
+              >
+                <NotificationOutlined id="notice-icon" /> 공지사항
               </div>
-              <Link to='/' className='link-to'>
-                <h4>로그아웃</h4>
-              </Link>
+
+              <div
+                className="navi-title"
+                onClick={() => navigate("/meeting/mancon")}
+              >
+                <YoutubeOutlined id="meet-icon" />회의생성
+              </div>
+
+              <div
+                className="navi-title"
+                onClick={() => navigate("/manager/productList")}
+              >
+                <BuildOutlined id="product-icon" /> 제품관리
+              </div>
+
+              <div
+                className="navi-title"
+                onClick={() => navigate(`/manager/mypage/${seq}`)}
+              >
+               <CgProfile id="mypage-icon" /> 마이 페이지
+              </div>
+
+              <div className="navi-title" onClick={onLogOutBtn}>
+              <LogoutOutlined id="logout-icon" />로그아웃
+              </div>
             </div>
           ) : (
-            <div className='navbar-right-navigator'>
-              <Link to='/manager/noticeList' className='link-to'>
-                <h4>공지사항</h4>
-              </Link>
-              <Link to='/meeting/mancon' className='link-to'>
-                <h4>회의 참여</h4>
-              </Link>
-              <div className='link-to' onClick={(e)=> navigate(`/consultant/mypage/${seq}`)}>
-                <h4>마이페이지</h4>
+            <div className="navbar-right-navigator">
+              <div
+                className="navi-title"
+                onClick={() => navigate("/manager/noticeList")}
+              >
+                <NotificationOutlined id="notice-icon" /> 공지사항
               </div>
-              <Link to='/' className='link-to'>
-                <h4>로그아웃</h4>
-              </Link>
+              <div
+                className="navi-title"
+                onClick={() => navigate("/meeting/mancon")}
+              >
+                <YoutubeOutlined id="meet-icon" />회의생성
+              </div>
+
+              <div
+                className="navi-title"
+                onClick={() => navigate(`/consultant/mypage/${seq}`)}
+              >
+               <CgProfile id="mypage-icon" /> 마이 페이지
+              </div>
+
+              <div className="navi-title" onClick={onLogOutBtn}>
+              <LogoutOutlined id="logout-icon" />로그아웃
+              </div>
             </div>
           )}
         </div>
