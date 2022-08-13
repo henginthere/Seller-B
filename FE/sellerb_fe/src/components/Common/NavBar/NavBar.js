c5eafd69be31e4bb4a8f45ed42df472151059bc1
임채현 <dlacogus5239@google.com>
2022-08-13 15:43:53 +0900

    [-][fe] style : 임시 배포

--- FE/sellerb_fe/src/components/Common/NavBar/NavBar.js
+++ FE/sellerb_fe/src/components/Common/NavBar/NavBar.js
@@ -3,15 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 import { getManagerInfoApi } from "../../../api/managerApi";
 import { detailConsultantApi } from "../../../api/consultantApi";
-import styled from 'styled-components'
-import 
-  { NotificationOutlined, YoutubeOutlined, 
-    DatabaseOutlined, LogoutOutlined,
-    BuildOutlined} from "@ant-design/icons";
+import styled from "styled-components";
+import {
+  NotificationOutlined,
+  YoutubeOutlined,
+  DatabaseOutlined,
+  LogoutOutlined,
+  BuildOutlined,
+} from "@ant-design/icons";
 import { CgProfile } from "react-icons/cg";
-
-
-
 
 function NavBar() {
   const navigate = useNavigate();
@@ -37,80 +37,90 @@ function NavBar() {
 
   return (
     <>
-      <div className="navbar-wrapper">
-        <div className="navbar-left">
+      <div className='navbar-wrapper'>
+        <div className='navbar-left'>
           <Link
             to={isManager ? "/manager/main" : "/consultant/main"}
             style={{ marginTop: "7px" }}
           >
             <img
-              className="navbar-logo-img"
-              alt="#"
+              className='navbar-logo-img'
+              alt='#'
               src={`${process.env.PUBLIC_URL}/img/sellerB_Logo.svg`}
             />
           </Link>
-          <div className="small-sellerb">SellerB</div>
+          <div className='small-sellerb'>SellerB</div>
         </div>
 
-        <div className="navbar-right">
+        <div className='navbar-right'>
           {isManager ? (
-            <div className="navbar-right-navigator">
+            <div className='navbar-right-navigator'>
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/manager/noticeList")}
               >
-                <NotificationOutlined id="notice-icon" /> 공지사항
+                <NotificationOutlined id='notice-icon' /> 공지사항
               </div>
 
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/meeting/mancon")}
               >
-                <YoutubeOutlined id="meet-icon" />회의생성
+                <YoutubeOutlined id='meet-icon' />
+                회의생성
               </div>
 
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/manager/productList")}
               >
-                <BuildOutlined id="product-icon" /> 제품관리
+                <BuildOutlined id='product-icon' /> 제품관리
               </div>
 
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate(`/manager/mypage/${seq}`)}
               >
-               <CgProfile id="mypage-icon" /> 마이 페이지
+                <CgProfile id='mypage-icon' /> 마이 페이지
               </div>
 
-              <div className="navi-title" onClick={onLogOutBtn}>
-              <LogoutOutlined id="logout-icon" />로그아웃
+              <div className='navi-title' onClick={onLogOutBtn}>
+                <LogoutOutlined id='logout-icon' />
+                로그아웃
               </div>
             </div>
           ) : (
-            <div className="navbar-right-navigator">
+            <div className='navbar-right-navigator'>
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/manager/noticeList")}
               >
-                <NotificationOutlined id="notice-icon" /> 공지사항
+                <NotificationOutlined id='notice-icon' /> 공지사항
               </div>
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/meeting/mancon")}
               >
-                <YoutubeOutlined id="meet-icon" />회의생성
+                <YoutubeOutlined id='meet-icon' />
+                회의생성
               </div>
-
               <div
-                className="navi-title"
+                className='navi-title'
+                onClick={() => navigate("/meeting/ConsultingMain")}
+              >
+                <YoutubeOutlined id='meet-icon' />
+                상담
+              </div>
+              <div
+                className='navi-title'
                 onClick={() => navigate(`/consultant/mypage/${seq}`)}
               >
-               <CgProfile id="mypage-icon" /> 마이 페이지
+                <CgProfile id='mypage-icon' /> 마이 페이지
               </div>
 
-              <div className="navi-title" onClick={onLogOutBtn}>
-              <LogoutOutlined id="logout-icon" />로그아웃
+              <div className='navi-title' onClick={onLogOutBtn}>
+                <LogoutOutlined id='logout-icon' />
+                로그아웃
               </div>
             </div>
           )}
