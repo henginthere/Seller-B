3684c8b66699904182e5e2312168578faebf7223
LJH <ljjuni1114@gmail.com>
2022-08-13 20:17:52 +0900

    [BE] feat : 기간별 상담 내역 조회 추가

--- BE/sellerB/src/main/java/backend/sellerB/repository/ConsultingRepository.java
+++ BE/sellerB/src/main/java/backend/sellerB/repository/ConsultingRepository.java
@@ -3,9 +3,14 @@ package backend.sellerB.repository;
 import backend.sellerB.entity.Consulting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ConsultingRepository extends JpaRepository<Consulting, Long> {
     List<Consulting> findAllByCustomer_CustomerId(String customerId);
     List<Consulting> findAllByConsultant_ConsultantId(String consultantId);
+
+    List<Consulting> findConsultingsByCustomer_CustomerIdAndAndConsultingStartDateIsAfter(String customerId, LocalDateTime consultingStartDate);
+
+    List<Consulting> findConsultingsByConsultant_ConsultantIdAndConsultingStartDateIsAfter(String ConsultantId, LocalDateTime consultingStartDate);
 }
