8017b75a171cf07dd23618e7a6ecf3670195b877
임채현 <dlacogus5239@google.com>
2022-08-14 23:00:28 +0900

    [-][fe] style : 상담 시작시 state waiting으로 변경 및 NavBar 조금 수정

--- FE/sellerb_fe/src/components/Common/NavBar/NavBar.js
+++ FE/sellerb_fe/src/components/Common/NavBar/NavBar.js
@@ -3,13 +3,16 @@ import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 import { getManagerInfoApi } from "../../../api/managerApi";
 import { detailConsultantApi } from "../../../api/consultantApi";
-import styled from 'styled-components'
-import 
-  { NotificationOutlined, YoutubeOutlined, 
-    DatabaseOutlined, LogoutOutlined,
-    BuildOutlined} from "@ant-design/icons";
+import styled from "styled-components";
+import {
+  NotificationOutlined,
+  YoutubeOutlined,
+  DatabaseOutlined,
+  LogoutOutlined,
+  BuildOutlined,
+  CustomerServiceOutlined,
+} from "@ant-design/icons";
 import { CgProfile } from "react-icons/cg";
-
 
 function NavBar() {
   const navigate = useNavigate();
@@ -18,9 +21,7 @@ function NavBar() {
 
   // 관리자, 상담사든 로그인할 때, 사용자의 seq받기
   useEffect(() => {
-
-    if (sessionStorage.getItem("adminCheck") === "ROLE_ADMIN") 
-    {
+    if (sessionStorage.getItem("adminCheck") === "ROLE_ADMIN") {
       setIsManager(true);
     } else {
       setIsManager(false);
@@ -31,89 +32,101 @@ function NavBar() {
   }, []);
 
   const onLogOutBtn = () => {
-    sessionStorage.removeItem("accessToken");
+    sessionStorage.clear();
+    navigate("/");
   };
-  
+
   const onGoMain = () => {
-    if(isManager){
-      navigate('/manager/main');
-    }else {
-      navigate('/consultant/main');
+    if (isManager) {
+      navigate("/manager/main");
+    } else {
+      navigate("/consultant/main");
     }
-  }
+  };
 
   return (
     <>
-      <div className="navbar-wrapper">
-        <div className="navbar-left">
-            <img
-              onClick={onGoMain}
-              className="navbar-logo-img"
-              alt="#"
-              src={`${process.env.PUBLIC_URL}/img/sellerB_Logo.svg`}
-            />
-          <div className="small-sellerb" onClick={onGoMain}>SellerB</div>
+      <div className='navbar-wrapper'>
+        <div className='navbar-left'>
+          <img
+            onClick={onGoMain}
+            className='navbar-logo-img'
+            alt='#'
+            src={`${process.env.PUBLIC_URL}/img/sellerB_Logo.svg`}
+          />
+          <div className='small-sellerb' onClick={onGoMain}>
+            SellerB
+          </div>
         </div>
 
-        <div className="navbar-right">
+        <div className='navbar-right'>
           {isManager ? (
-            <div className="navbar-right-navigator">
+            <div className='navbar-right-navigator'>
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/manager/noticeList")}
               >
-                <NotificationOutlined id="notice-icon" /> 공지사항
+                <NotificationOutlined id='notice-icon' /> 공지사항
               </div>
 
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/meeting/mancon")}
               >
-                <YoutubeOutlined id="meet-icon" />회의생성
+                <YoutubeOutlined id='meet-icon' />
+                회의생성
               </div>
 
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/manager/productList")}
               >
-                <BuildOutlined id="product-icon" /> 제품관리
+                <BuildOutlined id='product-icon' /> 제품관리
               </div>
 
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate(`/manager/mypage/${seq}`)}
               >
-               <CgProfile id="mypage-icon" /> 마이 페이지
+                <CgProfile id='mypage-icon' /> 마이 페이지
               </div>
 
-              <div className="navi-title" onClick={onLogOutBtn}>
-              <LogoutOutlined id="logout-icon" />로그아웃
+              <div className='navi-title' onClick={onLogOutBtn}>
+                <LogoutOutlined id='logout-icon' />
+                로그아웃
               </div>
             </div>
           ) : (
-            <div className="navbar-right-navigator">
+            <div className='navbar-right-navigator'>
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/manager/noticeList")}
               >
-                <NotificationOutlined id="notice-icon" /> 공지사항
+                <NotificationOutlined id='notice-icon' /> 공지사항
               </div>
               <div
-                className="navi-title"
+                className='navi-title'
                 onClick={() => navigate("/meeting/mancon")}
               >
-                <YoutubeOutlined id="meet-icon" />회의생성
+                <YoutubeOutlined id='meet-icon' />
+                회의참가
               </div>
-
               <div
-                className="navi-title"
+                className='navi-title'
+                onClick={() => navigate("/meeting/consultingMain")}
+              >
+                <CustomerServiceOutlined id='notice-icon' /> 상담
+              </div>
+              <div
+                className='navi-title'
                 onClick={() => navigate(`/consultant/mypage/${seq}`)}
               >
-               <CgProfile id="mypage-icon" /> 마이 페이지
+                <CgProfile id='mypage-icon' /> 마이 페이지
               </div>
 
-              <div className="navi-title" onClick={onLogOutBtn}>
-              <LogoutOutlined id="logout-icon" />로그아웃
+              <div className='navi-title' onClick={onLogOutBtn}>
+                <LogoutOutlined id='logout-icon' />
+                로그아웃
               </div>
             </div>
           )}
