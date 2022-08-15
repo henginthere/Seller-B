4c8f9dc13e20ca71e429bf6fea6b196bf2c6eb87
Gideok <76939190+GideokLee@users.noreply.github.com>
2022-08-16 03:11:15 +0900

    [S07P12D105-122][AOS] feat: 상담 내역 프래그먼트 추가

--- AOS/app/src/main/java/com/ssafy/sellerb/ui/main/MainActivity.kt
+++ AOS/app/src/main/java/com/ssafy/sellerb/ui/main/MainActivity.kt
@@ -1,32 +1,19 @@
 package com.ssafy.sellerb.ui.main
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import com.ssafy.sellerb.R
 import com.ssafy.sellerb.databinding.ActivityMainBinding
 import com.ssafy.sellerb.di.component.ActivityComponent
 import com.ssafy.sellerb.ui.base.BaseActivity
-import com.ssafy.sellerb.util.Constants.CHANNEL_ID
-import com.ssafy.sellerb.util.Constants.EXTRA_KEY_CONSULTING_SEQ
 import javax.inject.Inject
 
 
-class MainActivity : BaseActivity<MainViewModel>(){
+class MainActivity : BaseActivity<MainViewModel>() {
 
     lateinit var binding: ActivityMainBinding
 
@@ -44,15 +31,15 @@ class MainActivity : BaseActivity<MainViewModel>(){
     @Inject
     lateinit var mainSharedViewModel: MainSharedViewModel
 
-    override fun injectDependencies(activityComponent: ActivityComponent)
-    = activityComponent.inject(this)
+    override fun injectDependencies(activityComponent: ActivityComponent) =
+        activityComponent.inject(this)
 
     override fun setUpView(savedInstanceState: Bundle?) {
         navController = findNavController(R.id.container)
 
         binding.bottomNav.setupWithNavController(navController)
 
-        navController.addOnDestinationChangedListener{ _, destination, _ ->
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             onDestinationChanged(destination)
         }
 
@@ -60,20 +47,17 @@ class MainActivity : BaseActivity<MainViewModel>(){
             navController.navigate(R.id.item_home)
         }
 
-        val consultingSeq = intent!!.getLongExtra(EXTRA_KEY_CONSULTING_SEQ,0L)
-        Log.e(TAG,"CONSULTING_SEQ : " + consultingSeq)
-        mainSharedViewModel.consultingSeq.postValue(consultingSeq)
     }
 
 
-
-    private fun onDestinationChanged(destination: NavDestination){
-        when(destination.id){
+    private fun onDestinationChanged(destination: NavDestination) {
+        when (destination.id) {
             R.id.item_home,
-            R.id.item_my_page ->{
+            R.id.item_consulting_history,
+            R.id.item_my_page -> {
                 binding.coordinator.visibility = View.VISIBLE
             }
-            else ->{
+            else -> {
                 binding.coordinator.visibility = View.GONE
             }
         }
@@ -81,14 +65,6 @@ class MainActivity : BaseActivity<MainViewModel>(){
 
     override fun onNavigateUp(): Boolean {
         return navController.navigateUp() || super.onNavigateUp()
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        val consultingSeq = intent!!.getLongExtra(EXTRA_KEY_CONSULTING_SEQ,0L)
-        Toast.makeText(this,"LONG  " + consultingSeq, Toast.LENGTH_SHORT).show()
-        Log.e(TAG,"CONSULTING_SEQ : " + consultingSeq)
-        mainSharedViewModel.consultingSeq.postValue(consultingSeq)
-        super.onNewIntent(intent)
     }
 
 }
--- AOS/app/src/main/java/com/ssafy/sellerb/ui/main/MainViewModel.kt
+++ AOS/app/src/main/java/com/ssafy/sellerb/ui/main/MainViewModel.kt
@@ -2,7 +2,7 @@ package com.ssafy.sellerb.ui.main
 
 import com.ssafy.sellerb.data.repository.UserRepository
 import com.ssafy.sellerb.ui.base.BaseViewModel
-import com.ssafy.sellerb.util.CoroutineDispatchers
+import com.ssafy.sellerb.util.coroutine.CoroutineDispatchers
 import com.ssafy.sellerb.util.network.NetworkHelper
 
 class MainViewModel(
