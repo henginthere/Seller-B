b73c02737028260392573713be2ca4514ce58f0f
MilanoBeer <tgd5753@gmail.com>
2022-08-16 14:46:06 +0900

    [][FE] feat : 관리자 가입 프로필 이미지 업로드 부분 추가 & 비밀번호 재설정 제거 & 관리자 마이페이지 이미지 불러오기

--- FE/sellerb_fe/src/pages/Manager/Product/ProductRegister.js
+++ FE/sellerb_fe/src/pages/Manager/Product/ProductRegister.js
@@ -27,10 +27,8 @@ function ProductRegister() {
       
       return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, "$1,");
     };
-
     const uncomma = (str) => {
       str = String(str); 
-      
       return str.replace(/[^\d]+/g, "");
     };
 
