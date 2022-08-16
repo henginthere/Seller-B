a250f180b21202648914b66c10e8533e869c7651
신혜연 <tgd_5753@naver.com>
2022-08-17 02:17:19 +0900

    [][FE] fix : 제품 수정 API

---
+++ FE/sellerb_fe/src/components/Common/BackSmallButton.js
@@ -0,0 +1,65 @@
+import styled, { css } from "styled-components";
+
+const SIZE = {
+  sm: css`
+  --button-font-size: 0.825rem;
+  --button-padding: 8px 12px;
+  --button-radius: 4px; 
+  --button-width: 120px;
+  --button-height: 30px;
+  `,
+  md: css`
+  --button-font-size: 1rem;
+  --button-padding: 12px 16px;
+  --button-radius: 8px;
+  --button-width: 148px;
+  --button-height: 42px;
+  `,
+  lg: css`
+  --button-font-size: 1.25rem;
+  --button-padding: 16px 20px;
+  --button-radius: 12px;
+  `
+}
+// width: 148px;
+// height: 42px;
+const StyledButton = styled.button`
+  ${(p) => p.sizeStyle}
+
+  display: block;
+  padding: 0px 10px;
+  text-align: center;
+  overflow: hidden;
+
+
+  border-radius: 0px;
+  color: #132506;
+  background-color: #e0d5ed;
+  border: 0px none;
+  margin-right: 10px;
+
+  transition-duration: 400ms;
+  transition-timing-function: ease;
+  transition-property: transform;
+
+  &:hover {
+    transform: scale(1.04);
+    background-color: #e9d9ee;
+    cursor: pointer;
+  }
+
+
+`;
+
+export const BackSmallButton = ({disabled, size, label, onClick}) => {
+  // 상단 사이즈 리스트에서 해당하는 것 가져옴
+  const sizeStyle= SIZE[size];
+
+  return ( 
+    <StyledButton disabled={disabled} sizeStyle={size} onClick={()=> onClick()}>
+      {label}
+    </StyledButton>
+  );
+}
+
+// export default MyButton;
