5c5688a80d477d4aa8e5b6485c284a02692dd2c0
임채현 <dlacogus5239@google.com>
2022-08-18 16:03:19 +0900

    [-][fe] style : 최종수정

--- FE/sellerb_fe/src/api/userApi.js
+++ FE/sellerb_fe/src/api/userApi.js
@@ -1,27 +1,24 @@
 import api from "./api";
-<<<<<<< HEAD
-import fileapi from "./fileapi";
-import axios from 'axios';
-=======
-// import axios from "axios";
->>>>>>> feature-ui-FinalFIx
 
 export const LoginApi = async (userInfo, success, fail) => {
   return await api.post("/auth/login");
 };
 
-export const registerApi = async(userInfo, success, fail) => {
-    console.log("in registerAPI :" + JSON.stringify(userInfo))
-    return await api.post("/manager/register", userInfo).then(success).catch(fail);
-}
+export const registerApi = async (userInfo, success, fail) => {
+  console.log("in registerAPI :" + JSON.stringify(userInfo));
+  return await api
+    .post("/manager/register", userInfo)
+    .then(success)
+    .catch(fail);
+};
 
-export const modifyManagerApi = async(userInfo, seq, success, fail) => {
-    return await api
-        .put(`/manager/${seq}`, userInfo, {
-            header:{
-                "Content-Type": `application/json`,
-            },
-        })
-        .then(success)
-        .catch(fail);
-}
+export const modifyManagerApi = async (userInfo, seq, success, fail) => {
+  return await api
+    .put(`/manager/${seq}`, userInfo, {
+      header: {
+        "Content-Type": `application/json`,
+      },
+    })
+    .then(success)
+    .catch(fail);
+};
