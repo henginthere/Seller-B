15e46d5630934d9edb137d52dc1fd8e112a3b87b
임채현 <dlacogus5239@google.com>
2022-08-18 16:01:31 +0900

    [-][fe] style : 최종수정

--- FE/sellerb_fe/src/components/Common/DangerMediButton.js
+++ FE/sellerb_fe/src/components/Common/DangerMediButton.js
@@ -1,23 +1,23 @@
 import styled, { css } from "styled-components";
 
-const SIZE = {
-  sm: css`
-  --button-font-size: 0.825rem;
-  --button-padding: 8px 12px;
-  --button-radius: 4px; 
-  `,
-  md: css`
-  --button-font-size: 1rem;
-  --button-padding: 12px 16px;
-  --button-radius: 8px;
-  `,
-  lg: css`
-  --button-font-size: 1.25rem;
-  --button-padding: 16px 20px;
-  --button-radius: 12px;
-  `
-}
-// width: 148px;
+// const SIZE = {
+//   sm: css`
+//     --button-font-size: 0.825rem;
+//     --button-padding: 8px 12px;
+//     --button-radius: 4px;
+//   `,
+//   md: css`
+//     --button-font-size: 1rem;
+//     --button-padding: 12px 16px;
+//     --button-radius: 8px;
+//   `,
+//   lg: css`
+//     --button-font-size: 1.25rem;
+//     --button-padding: 16px 20px;
+//     --button-radius: 12px;
+//   `,
+// };
+// // width: 148px;
 // height: 42px;
 const StyledButton = styled.button`
   ${(p) => p.sizeStyle}
@@ -30,7 +30,7 @@ const StyledButton = styled.button`
   height: 42px;
 
   border-radius: 0px;
- color: #edefe9;
+  color: #edefe9;
   background-color: #ff7d68;
   border: 0px none;
   margin-right: 10px;
@@ -44,22 +44,21 @@ const StyledButton = styled.button`
     background-color: #923c35;
     cursor: pointer;
   }
-
-
-
 `;
 
-export const DangerMediButton = ({disabled, size, label, onClick}) => {
-
-
+export const DangerMediButton = ({ disabled, size, label, onClick }) => {
   // 상단 사이즈 리스트에서 해당하는 것 가져옴
-//   const sizeStyle= SIZE[size];
+  //   const sizeStyle= SIZE[size];
 
-  return ( 
-    <StyledButton disabled={disabled} sizeStyle={size} onClick={()=> onClick()}>
+  return (
+    <StyledButton
+      disabled={disabled}
+      sizeStyle={size}
+      onClick={() => onClick()}
+    >
       {label}
     </StyledButton>
   );
-}
+};
 
 // export default MyButton;
