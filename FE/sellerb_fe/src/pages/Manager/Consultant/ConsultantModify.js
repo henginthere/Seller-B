b4ff15bea0acc40ae440dfdfd57c0ddb3845d4c8
MilanoBeer <tgd5753@gmail.com>
2022-08-18 10:12:22 +0900

    [][FE] fix : [관리자] 상담원 수정

--- FE/sellerb_fe/src/pages/Manager/Consultant/ConsultantModify.js
+++ FE/sellerb_fe/src/pages/Manager/Consultant/ConsultantModify.js
@@ -6,24 +6,30 @@ import Button from "@mui/material/Button";
 import { SmallButton } from "../../../components/Common/SmallButton";
 import { MediButton } from "../../../components/Common/MediButton";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { productGroupListApi } from "../../../api/productApi";
+import { DangerSmallButton } from '../../../components/Common/DangerSmallButton'
 import { DangerMediButton} from '../../../components/Common/DangerMediButton'
-
+import AttendanceLog from "../../../components/Log/AttendanceLog";
+import ConsultingLog from "../../../components/Log/ConsultingLog";
+import { productGroupListApi } from "../../../api/productApi";
 import {
   modifyConsultantApi,
   detailConsultantApi,
 } from "../../../api/consultantApi";
-
 function ConsultantModify() {
   const navigate = useNavigate();
   const { id } = useParams();
   const seq = id;
-
-  const [consultant, setConsultant] = useState({});
+  const [editPass, setEditPass] = useState();
+  const [consultant, setConsultant] = useState([]);
   const [groupList, setGroupList] = useState([]);
-  const [managerBrand, setManagerBrand] = useState(
-    sessionStorage.getItem("brandNameKor"),
-  );
+  const [logOption, setLogOption] = useState("출결이력");
+  const product_list = [
+    { value: "TV", label: "TV" },
+    { value: "phone", label: "Phone" },
+    { value: "airConditioner", label: "에어컨" },
+    { value: "Refrigerator", label: "냉장고" },
+    { value: "airCleaner", label: "공기청정기" },
+  ];
 
   useEffect(() => {
     if (sessionStorage.getItem("accessToken") === null) {
@@ -46,44 +52,171 @@ function ConsultantModify() {
 
   useEffect(()=>{
     productGroupListApi()
-    .then((res) => {
-      setGroupList(res.data); // groupList
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .then((res) => {
+        setGroupList(res.data); // groupList
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [])
 
   // 최종적으로 수정 완료 버튼을 누르면 api로 DB에 반영하기 위한 함수
-  const onHandleSubmit = (e) => {
-    e.preventDefault();
+  // const onHandleSubmit = (e) => {
+  //   e.preventDefault();
+    
+  //   console.log("컨설턴트 수정 :" + JSON.stringify(consultant))
 
-    modifyConsultantApi(consultant.consultantSeq, consultant)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log("Error");
-      });
+  //   modifyConsultantApi(consultant.consultantSeq, consultant)
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log("Error");
+  //     });
 
-    alert("수정하시겠습니까?");
+  //   alert("수정하시겠습니까?");
 
-    navigate(`/manager/consultantDetail/${seq}`);
-  };
-
+  //   navigate(`/manager/consultantDetail/${seq}`);
+  // };
+  // 바뀔때마다 setData로 수정된 데이터로 바꿔줌
   const onChange = (e) => {
     e.preventDefault();
-    const { value, name } = e.target;
 
+    const { value, name } = e.target;
     setConsultant({
       ...consultant,
       [name]: value,
     });
   };
+
+  function ConsultantLog(props) {
+    if (logOption === "출결이력") {
+      // console.log(params.consultants
+      return <AttendanceLog consultant_id={seq} />;
+    } else {
+      return <ConsultingLog consultant_id={seq} />;
+    }
+  }
+
+  // 수정완료
+  const onEditCompleteBtn = async () => {
+    const EditInfo = {
+      consultantPass: editPass,
+      consultantTel: consultant.consultantTel,
+      productGroupSeq: consultant.consultantEmail,
+      consultantImageUrl: consultant.consultantImageUrl,
+    };
+
+    modifyConsultantApi(seq, EditInfo)
+    .then((res)=>{
+      console.log(JSON.stringify(res.data));
+      console.log("success");
+    })
+    .catch((err)=>{
+
+      console.log("Error");
+    })
+
+    navigate("/main");
+  };
+
+  const onHandleLogOption = (event) => {
+    setLogOption(event.currentTarget.value);
+
+    console.log(logOption);
+  };
+
+  const onDeleteBtn = () =>{
+    detailConsultantApi(seq)
+    .then((res)=>{
+      console.log("Success");
+    })
+    .catch((err)=>{
+      console.log("Error");
+    })
+  }
+
+    // editPass Handling
+    const onChangePass = (e) => {
+      setEditPass(e.target.value);
+    };
+  // state Handling
+  const onChangeInfo = (e) => {
+    const { name, value } = e.target;
+    setConsultant({
+      ...consultant,
+      [name]: value,
+    });
+  };
+
+
   return (
     <>
       <NavBar />
-      <div className='wrapper'>
+      <div className="notice-title">상담사 프로필</div>
+      {/*  */}
+      <div className="consultant-profile-container">
+        <div className="profile-wrapper">
+          <div className="con-mypage-left-wrapper">
+            <img
+              className="con-mypage-default-img"
+              alt="#"
+              src={consultant.consultantImageUrl}
+            />
+          </div>
+        {/*  */}
+        <div className="con-mypage-profile-left">
+        <div className="con-profile-element">
+            <p>사번</p>
+            <div>{consultant.consultantId}</div>
+          </div>
+          <div className="con-profile-element">
+            <p>사원명</p>
+            <div>{consultant.consultantName}</div>
+          </div>
+          <div className="con-profile-element">
+            <p>사원 Email</p>
+            <input
+                defaultValue={consultant.consultantEmail}
+                name='consultantEmail'
+                onChange={onChangeInfo}
+              />
+          </div>
+          <div className="con-profile-element">
+           <p>사원 Pnum</p>
+           <input
+                defaultValue={consultant.consultantTel}
+                name='consultantTel'
+                onChange={onChangeInfo}
+              />
+          </div>
+          <div className="con-profile-element">
+            <p>제품군</p>
+            <input value={editPass} onChange={onChangePass} />
+          </div>         
+          <div style={{display:"flex", width:"300px"}}>
+            <SmallButton label="수정완료" onClick={onEditCompleteBtn} />
+            <DangerSmallButton label="삭제하기" onClick={onDeleteBtn} />
+          </div>
+        </div>
+        {/*  */}
+        </div>
+        <div className="profile-right">
+          <div className="consultant-detail-select-wrapper">
+
+            <select onChange={onHandleLogOption} value={logOption}>
+              <option>출결이력</option>
+              <option>상담이력</option>
+            </select>
+          </div>
+          <div className='attendance-log'>
+            <ConsultantLog consultant_id={seq} />
+          </div>
+      </div>
+
+
+      </div>
+      {/* <div className='wrapper'>
         <div id='left'>
           <div className='imageWrapper'>
             <img src={consultant.consultantImageUrl} alt='NOIMAGE'></img>
@@ -95,13 +228,12 @@ function ConsultantModify() {
           </div>
           <form className='InfoWrapper'>
             <div className='registerField'>
-              {/* 이 부분 Form 으로 바꿔주기 */}
               <TextField
                 required
                 value={consultant.consultantId || ""}
                 fullWidth='true'
                 name='consultantId'
-                // onChange={onChange}
+                onChange={onChange}
               />
             </div>
             <div className='registerField'>
@@ -111,7 +243,7 @@ function ConsultantModify() {
                 value={consultant.consultantName || ""}
                 fullWidth='true'
                 name='consultantName'
-                // onChange={onChange}
+                onChange={onChange}
               />
             </div>
             <div className='registerField'>
@@ -151,21 +283,25 @@ function ConsultantModify() {
                 required
                 select
                 fullWidth='true'
-                name='productGroupName'
+                name='productGroupSeq'
                 value={consultant.productGroupName}
                 selected={consultant.productGroupName}
+                SelectProps={{
+                  native: true,
+                }}
                 onChange={onChange}
               >
-                <option value='' />
-                {groupList.map((option) =>
-                  option.brandName === managerBrand ? (
-                    <option>{option.productGroupName}</option>
-                  ) : (
+                {product_list.map((option) =>
+                  option.value === consultant.productGroupName ? (
                     ""
+                  ) : (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
                   ),
                 )}
-              </TextField>
-            </div>
+              </TextField> */}
+            {/* </div>
             <div className='Button'>
               <MediButton
                 label="수정완료"
@@ -173,11 +309,12 @@ function ConsultantModify() {
                 className='registerBtn'
                 variant='contained'
                />
+             
               <DangerMediButton className='registerBtn' label="취소" variant='contained' color='error' />
             </div>
           </form>
         </div>
-      </div>
+      </div> */}
       <Footer />
     </>
   );
